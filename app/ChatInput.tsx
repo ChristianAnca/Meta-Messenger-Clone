fcd68d039de7e79dc0921c0af952ec2f18f15f79
Christian Anca <christiananca@Christians-MacBook-Pro.local>
2023-03-12 21:06:55 -0400

    uuid implimented

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React, { FormEvent } from 'react'
 import { useState } from 'react'
+import { v4 as uuid} from 'uuid';
+import { Message } from '../typings';
 
 
 function ChatInput() {
@@ -15,7 +17,19 @@ function ChatInput() {
         const messageToSend = input;
 
         setInput('');
-    }
+
+        const id = uuid();
+
+        const message : Message = {
+            id: id,
+            message: messageToSend,
+            created_at: Date.now(),
+            username: 'Elon Musk',
+            profilePic: 'https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg',
+            email: 'hansanca@gmail.com'
+        }
+        
+    };
 
   return (
     <form onSubmit={addMessage} className='fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100'>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['links.papareact.com']
+    domains: ['links.papareact.com', 'pintrest.ph']
   },
   experimental: {
     appDir: true,
